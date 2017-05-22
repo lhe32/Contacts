f044b43ca26ad860c9991ed7656b879d60a73a2c
lhe32 <lhe@sageridge.org>
2017-05-21 23:10:28 -0700

    Add files via upload

---
+++ ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Table View
+//
+//  Created by Detective Lawliet on 5/18/17.
+//  Copyright © 2017 Detective Lawliet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *namesList;
+
+@end
+
